9b37babb697ffb25992d8517a778710e331a8691
David Salamon <salamon.david@gmail.com>
2016-11-21 18:15:59 +0100

    Grunt basics (coffee)

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+  /** 
+   * Load required Grunt tasks. These are installed based on the versions listed
+   * in `package.json` when you do `npm install` in this directory.
+   */
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  
+  /**
+   * Load in our build configuration file.
+   */
+  var userConfig = require( './build.config.js' );
+
+  /**
+   * This is the configuration object Grunt uses to give each plugin its 
+   * instructions.
+   */
+  var taskConfig = {
+    /**
+     * We read in our `package.json` file so we can access the package name and
+     * version. It's already there, so we don't repeat ourselves here.
+     */
+    pkg: grunt.file.readJSON("package.json"),
+    
+    /**
+     * The banner is the comment that is placed at the top of our compiled 
+     * source files. It is first processed as a Grunt template, where the `<%=`
+     * pairs are evaluated based on this very configuration object.
+     */
+    meta: {
+      banner: 
+        '/**\n' +
+        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        ' * <%= pkg.homepage %>\n' +
+        ' *\n' +
+        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+        ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
+        ' */\n'
+    },
+
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // },
+    
+    coffee: {
+      source: {
+        options: {
+          bare: true
+        },
+        expand: true,
+        cwd: '.',
+        src: [ '<%= app_files.coffee %>' ],
+        dest: '<%= build_dir %>',
+        ext: '.js'
+      }
+    },
+
+    
+  };
+  
+  // Default task(s).
+  grunt.registerTask('default', ['coffee']);
+
+  grunt.initConfig( grunt.util._.extend( taskConfig, userConfig ) );
+
+
+};
---
+++ build.config.js
@@ -0,0 +1,26 @@
+/**
+ * This file/module contains all configuration for the build process.
+ */
+module.exports = {
+  /**
+   * The `build_dir` folder is where our projects are compiled during
+   * development and the `compile_dir` folder is where our app resides once it's
+   * completely built.
+   */
+  build_dir: 'build',
+  compile_dir: 'bin',
+
+  /**
+   * This is a collection of file patterns that refer to our app code (the
+   * stuff in `src/`). These file paths are used in the configuration of
+   * build tasks. `js` is all project javascript, less tests. `ctpl` contains
+   * our reusable components' (`src/common`) template HTML files, while
+   * `atpl` contains the same, but for our app's code. `html` is just our
+   * main HTML file, `less` is our main stylesheet, and `unit` contains our
+   * app's unit tests.
+   */
+  app_files: {
+    coffee: [ 'src/**/*.js.coffee', 'src/**/*.coffee' ],
+  },
+
+};
