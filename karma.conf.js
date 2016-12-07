736ebbba01602790744d46d87f09d267c47f1c27
Bence Nagy <bence.nagy@virtualconstructor.com>
2016-12-07 10:33:05 +0100

    Create (local) test coverage info on test run

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.haml': ['ng-haml2js']
+      '**/*.haml': ['ng-haml2js'],
+      './bin/assets/cloudstorm.js': ['coverage']
     },
 
     ngHaml2JsPreprocessor: {
@@ -44,10 +45,15 @@ module.exports = function(config) {
       moduleName: 'hamlTemplates'
     },
 
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
+    },
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage'],
 
     // web server port
     port: 9876,
