ae3c69088f6232608a6813778625bfc473da14c8
Bence Nagy <bence.nagy@virtualconstructor.com>
2016-12-08 11:14:40 +0100

    Simplify karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,83 +1,76 @@
-module.exports = function(config) {
-  config.set({
+// module.exports = function(config) {
+//   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+//     // base path that will be used to resolve all patterns (eg. files, exclude)
+//     basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+//     // frameworks to use
+//     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+//     frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      //Setup:
-      './node_modules/angular/angular.js',                             // angular
-      './node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js', // bootstrap
-      './node_modules/underscore/underscore-min.js',                   // underscore
-      './node_modules/angular-mocks/angular-mocks.js',                 // loads our modules for tests
-      './bin/assets/cloudstorm.js',                                    // cloudstorm itself
-      // if you wanna load template files in nested directories, you must use this:
-      './src/**/*.haml',
+//     // list of files / patterns to load in the browser
+//     files: [
+//       //Setup:
+//       './node_modules/angular/angular.js',                             // angular
+//       './node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js', // bootstrap
+//       './node_modules/underscore/underscore-min.js',                   // underscore
+//       './node_modules/angular-mocks/angular-mocks.js',                 // loads our modules for tests
+//       './bin/assets/cloudstorm.js',                                    // cloudstorm itself
+//       // HAML templates (there is a preprocess step injecting them in the template cache)
+//       './src/**/*.haml',
+//       // Tests
+//       './src/**/*.spec.js'
+//     ],
 
-      // Tests:
-      './src/components/cs-alert/cs-alert-service.spec.js',
-      './src/components/cs-alert/cs-alert.spec.js'
-    ],
+//     // list of files to exclude
+//     exclude: [
+//     ],
 
-    // list of files to exclude
-    exclude: [
-    ],
+//     // preprocess matching files before serving them to the browser
+//     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+//     preprocessors: {
+//       './src/**/*.haml': ['ng-haml2js'],
+//       './bin/assets/cloudstorm.js': ['coverage']
+//     },
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/*.haml': ['ng-haml2js'],
-      './bin/assets/cloudstorm.js': ['coverage']
-    },
+//     ngHaml2JsPreprocessor: {
+//       stripPrefix: 'src/',
+//       moduleName: 'hamlTemplates'
+//     },
 
-    ngHaml2JsPreprocessor: {
-      stripPrefix: 'src/',
+//     coverageReporter: {
+//       type : 'lcov',
+//       dir : 'coverage/'
+//     },
 
-      // cacheIdFromPath: function(filepath) {
-      //   // remove 'src/' from the start of the filename so the template cache works:
-      //   return filepath.substring(4);
-      // },
-      moduleName: 'hamlTemplates'
-    },
+//     // test results reporter to use
+//     // possible values: 'dots', 'progress'
+//     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+//     reporters: ['spec', 'coverage'],
 
-    coverageReporter: {
-      type : 'lcov',
-      dir : 'coverage/'
-    },
+//     // web server port
+//     port: 9876,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'coverage'],
+//     // enable / disable colors in the output (reporters and logs)
+//     colors: true,
 
-    // web server port
-    port: 9876,
+//     // level of logging
+//     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+//     logLevel: config.LOG_INFO,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+//     // enable / disable watching file and executing tests whenever any file changes
+//     autoWatch: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+//     // start these browsers
+//     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+//     browsers: ['PhantomJS'],
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+//     // Continuous Integration mode
+//     // if true, Karma captures browsers, runs the tests and exits
+//     singleRun: false,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+//     // Concurrency level
+//     // how many browser should be started simultaneous
+//     concurrency: Infinity
+//   })
+// }
