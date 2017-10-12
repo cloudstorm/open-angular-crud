ba2810dea6ce482c80b24095cc0820ce2464437e
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-10-12 17:01:16 +0200

    The component containing the profile page or the cs-wizard loads starts to work. The submission and closing handling has to be done later.

--- src/cs-route-provider/templates/showNew/cs-route-show_new.js
+++ src/cs-route-provider/templates/showNew/cs-route-show_new.js
@@ -8,9 +8,31 @@ app.component("csUIRouteShowNew", {
       id : '<',
       resourceType : '<',
     },
-    templateUrl : 'cs-route-provider/templates/showNew/cs-route-showNew-template.html'
+    templateUrl : 'cs-route-provider/templates/showNew/cs-route-showNew-template.html',
+    controller : function($scope, csRoute, csDescriptorService){
+      
+      this.ready = false
+      csDescriptorService.getPromises().then((function(){
+        //The form config object
+        this.ready = true
+        console.log("1")
+        this.wizardOptions = {
+          "resource-type" : "items",
+          "form-item": {},
+          "form-mode": "create",
+          "reset-on-submit": true,
+          "events": {
+            'wizard-canceled': function(resource){
+                csRoute.go()
+              },
+            'wizard-submited': function(resource){
+              pushNewItem($scope.collection, resource)
+              csRoute.go()
+            }
+          }
+        }
+        $scope.$apply()
+      }).bind(this))
+    }
  })
-
-
-
 
