631eb1fde5a0f163ed608f175acbbc4aa249e451
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-10-13 08:58:00 +0200

    cs-route-show_new component handles if the there is no such resource which has been given in the url.

--- src/cs-route-provider/templates/showNew/cs-route-show_new.js
+++ src/cs-route-provider/templates/showNew/cs-route-show_new.js
@@ -9,28 +9,36 @@ app.component("csUIRouteShowNew", {
       resourceType : '<',
     },
     templateUrl : 'cs-route-provider/templates/showNew/cs-route-showNew-template.html',
-    controller : function($scope, csRoute, csDescriptorService){
+    controller : function($scope, csRoute, csDescriptorService, ResourceService){
       
       this.ready = false
       csDescriptorService.getPromises().then((function(){
         //The form config object
         this.ready = true
-        console.log("1")
-        this.wizardOptions = {
-          "resource-type" : "items",
-          "form-item": {},
-          "form-mode": "create",
-          "reset-on-submit": true,
-          "events": {
-            'wizard-canceled': function(resource){
-                csRoute.go()
-              },
-            'wizard-submited': function(resource){
-              pushNewItem($scope.collection, resource)
-              csRoute.go()
+        this.found = true
+        try {
+          ResourceService.get(this.resourceType)
+        } catch(err) {
+          console.log("There is no such resource")
+          this.found = false;
+        }
+        if(this.found){
+          this.wizardOptions = {
+              "resource-type" : this.resourceType,
+              "form-item": {},
+              "form-mode": "create",
+              "reset-on-submit": true,
+              "events": {
+                'wizard-canceled': function(resource){
+                    csRoute.go()
+                  },
+                'wizard-submited': function(resource){
+                  pushNewItem($scope.collection, resource)
+                  csRoute.go()
+                }
+              }
             }
           }
-        }
         $scope.$apply()
       }).bind(this))
     }
