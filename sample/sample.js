9e3197f818149f64c90a71a5911077ffbf2a050a
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-10-20 14:27:01 +0200

    The sample.js contains the URL state definitions. The new features that each state calls the csRoutePage component and the pageType variable holds the name of the state. Later it will be solved that the csRoutePage component gets the state from the csRoute provider.

--- sample/sample.js
+++ sample/sample.js
@@ -2,57 +2,65 @@ var app = angular.module('cloudStormSample', [
   'cloudStorm',
 ])
 
-
 var index = {
   name: 'type',
   url: '/{resourceType}',
-  component : "csUIRouteIndex", 
+  component : "csRoutePage",
   resolve : {
     resourceType : function($transition$){
       return $transition$.params().resourceType
-    }
+    },
+    pageType : function($transition$){
+      return "index"
+    },
   }
-}
-
-var edit = {
-    name : "id",
-    url : "/{resourceType}/{id}/{cmd}",
-    component : "csUIRouteEdit",
-     resolve : {
-       id : function($transition$){
-         return $transition$.params().id
-       },
-       resourceType : function($transition$){
-         return $transition$.params().resourceType
-       },  
-       cmd : function($transition$){
-         return $transition$.params().cmd
-       }
-    }
 }
 
 var show_new = {
   name : "show",
   url : "/{resourceType}/{id}",
-  component : "csUIRouteShowNew",
+  component : "csRoutePage",
   resolve : {
-      id : function($transition$){
-        return $transition$.params().id
-      },
       resourceType : function($transition$){
         return $transition$.params().resourceType
       },
+      id : function($transition$){
+        return $transition$.params().id
+      },
+      pageType : function($transition$){
+        return "profile"
+      },
    }
+ }
+
+var edit = {
+    name : "id",
+    url : "/{resourceType}/{id}/{cmd}",
+    component : "csRoutePage",
+     resolve : {
+       resourceType : function($transition$){
+         return $transition$.params().resourceType
+       },
+       id : function($transition$){
+         return $transition$.params().id
+       },
+       cmd : function($transition$){
+         return $transition$.params().cmd
+       },
+       pageType : function($transition$){
+         return "edit"
+       },
+    }
 }
 
 app.controller('MainCtrl', function($scope, csAlertService, csDescriptorService, csRoute, $state) {
-  
+
   csDescriptorService.registerDescriptorUrl('resourceDescriptors/itemResourceDescriptor.json');
   csDescriptorService.registerDescriptorUrl('resourceDescriptors/categoryResourceDescriptor.json');
   csDescriptorService.registerDescriptorUrl('resourceDescriptors/userResourceDescriptor.json');
-  
+
   csRoute.setState($state)
   csRoute.addState(index)
-  csRoute.addState(edit)
   csRoute.addState(show_new)
+  csRoute.addState(edit)
 });
