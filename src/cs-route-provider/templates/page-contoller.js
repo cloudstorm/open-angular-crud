fd4fb185e17371d541f6e39a241a051ea4caf80a
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-10-20 14:19:29 +0200

    The page controller handles the loading of the data of the particular pages, and prepares additional dataset which is required for certain components. Later it may loads the dataset based on a configuration dataset dynamically.

--- src/cs-route-provider/templates/page-contoller.js
+++ src/cs-route-provider/templates/page-contoller.js
@@ -5,9 +5,10 @@ app = angular.module("cloudStrom.uiRouterPageController", [])
 app.component("csRoutePage", {
 
     bindings : {
-        pageType : "<",
         resourceType : "<",
         id : "<",
+        cmd : "<",
+        pageType : "<",
     },
     templateUrl : "cs-route-provider/templates/router-page-template.html",
     controller : function($scope, $timeout, csRoute, ResourceService, csDataLoader, csDescriptorService){
@@ -15,123 +16,219 @@ app.component("csRoutePage", {
         this.testValue = "InitialValue"
         this.loading = true
         this.errors = []
-
         //getDataLoaderObject(this, descriptor)["index"].call()
+        this.init = function (){
+            switch (this.pageType) {
+              case "index": this.resource_index(); break;
+              case "edit":
+                if(this.cmd != "edit"){
+                  this.errors.push("\"" + this.cmd + "\" is not a valid command");
+                }
+                this.resource_id();
+                break;
+              case "profile":
+                if(this.id == "new"){
+                  this.pageType = "new"
+                  this.resource();
+                } else {
+                  this.resource_id();
+                }
+                break;
+              default:
+                this.errors.push("This is not a valid URL");
+                this.finished()
+                break;
+            }
+        }
 
-        var call = function(scope){
+        this.resource = function(){
 
           csDescriptorService.getPromises().then(
             (function(){
               return ResourceService.get(this.resourceType)
-            }).bind(scope)).then(
+            }).bind(this)).then(
+              (function(resource){
+                console.log(resource.descriptor)
+                this.resource = resource
+                this.finished()
+              }).bind(this), (function(){
+                this.errors.push("\"" + this.resourceType + "\" is not a resource")
+                this.finished()
+              }).bind(this)
+            )
+        }
+
+        this.resource_id = function(){
+
+          csDescriptorService.getPromises().then(
+            (function(){
+              return ResourceService.get(this.resourceType)
+            }).bind(this)).then(
               (function(resource){
                 console.log(resource.descriptor)
                 this.resource = resource
                 return resource.$get(this.id, {include: '*'})
-              }).bind(scope), (function(){
+              }).bind(this), (function(){
                 this.errors.push("\"" + this.resourceType + "\" is not a resource")
-              }).bind(scope)
+              }).bind(this)
             ).then((function(item){
                 this.item = item
                 this.finished()
-            }).bind(scope), (function(){
-              this.errors.push("There is no " + this.resource.descriptor.name + "with the id: " + this.id)
+            }).bind(this), (function(){
+              this.errors.push("There is no " + this.resource.descriptor.name + " with the id: " + this.id)
               this.finished()
-            }).bind(scope))
+            }).bind(this))
+        }
+
+        this.resource_index = function(){
+
+          csDescriptorService.getPromises()
+            .then((function(){
+              return ResourceService.get(this.resourceType)
+            }).bind(this))
+            .then(
+              (function(resource){
+                console.log(resource.descriptor)
+                this.resource = resource
+                return resource.$index({include: '*'})
+                this.finished()
+              }).bind(this), (function(){
+                this.errors.push("\"" + this.resourceType + "\" is not a resource")
+              }).bind(this))
+            .then((function(items){
+                  this.items = items
+                  this.finished()
+              }).bind(this)
+            )
         }
 
         this.finished = function(){
-
-          this.patternType = "edit"
-          this.wizardOptions = {
-            "resource-type" : this.resourceType,
-            "form-item": this.item,
-            "form-mode": "edit",
-            "reset-on-submit": true,
-            "events": {
-              'wizard-canceled': (function(resource){
-                  csRoute.go("type", {resourceType : this.resourceType})
+          //Prepare data for
+          if(this.pageType == "edit" || this.pageType == "new"){
+            var item = (this.pageType == "edit") ? this.item : {}
+            var mode = (this.pageType == "edit") ? "edit" : "create"
+            this.wizardOptions = {
+              "resource-type" : this.resourceType,
+              "form-item": item,
+              "form-mode": mode,
+              "reset-on-submit": true,
+              "events": {
+                'wizard-canceled': (function(resource){
+                    csRoute.go("type", {resourceType : this.resourceType})
+                  }).bind(this),
+                'wizard-submited': (function(resource){
+                    csRoute.go("type", {resourceType : this.resourceType})
                 }).bind(this),
-              'wizard-submited': (function(resource){
-                  csRoute.go("type", {resourceType : this.resourceType})
-              }).bind(this),
+              }
             }
           }
           this.loading = false
           $scope.$apply()
         }
-        call(this)
-
+        this.init()
       }
 })
 
-
 /*
-var descriptor = {
+//For later
+var functionStack = {
 
-    states : {
-      index : ["resource"],
-      new : ["resource"],
-      profile : ["resource", "item"],
-      edit : ["resource", "item"],
+    descriptor : {
+      call : {
+        type : "direct",
+        fcn : (csDescriptorService.getPromises).bind(csDescriptorService),
+      }, params : [],
+      success : function(){},
+      fail : function(){},
     },
-    calls : {
-      resource : {
-        call : {
-          type : "direct",
-          function : csDescriptorService.getPromises.bind(csDescriptorService),
-          params : [],
-        }, success : function(scope){
-            //How can I access here the injected service - complicated
-            scope.resource = ResourceService.get(scope.resourceType)
-          }
+    resource : {
+      call :  {
+        type : "direct",
+        fcn : (ResourceService.get).bind(ResourceService)
       },
-      item : {
-        call : {
-          type : "scopeField",
-          keys : ["resourceType", "$get"],
-          params : [{
-            type : "scopeField",
-            keys : ["itemId"],
-          }, {
-            type : "constant",
-            vale : { include : "*"}
-          }]
-        },
-        success : {
-          type : "function",
-        }
+      params : [{
+        type : "scopeField",
+        key : "resourceType"
+      }],
+      success : function(data){
+        this.resource = data
       },
-    }
-} */
-
-/*
-var getDataLoaderObject = function(scope, desc){
-
-    var calls = []
-    var arr = desc
-
-    var stateLoaders = new Object()
-    var loader1
-    var loader2
-    var callName
-    var callName1
-    var callName2
-    for(var state in desc.states){
-      if(desc.states[state].length == 1){
-        callName = desc.states[state][0]
-        stateLoaders[state] = csDataLoader.$new(scope, callName, desc.calls[callName])
-      } else {
-        for(var i = desc.states[state].length - 1; i > 0; i--){
-          callName1 = desc.states[state][i]
-          callName2 = desc.states[state][i - 1]
-          loader1 = csDataLoader.$new(scope, callName1, desc.calls[callName1])
-          loader2 = csDataLoader.$new(scope, callName2, desc.calls[callName2])
-          loader2.nextLoader(loader1)
-        }
-        stateLoaders[state] = loader2
+      fail : function(){
+        this.errors.push("\"" + this.resourceType + "\" is not a resource")
+      }
+    },
+    item : {
+      call : {
+        type : "scopeField",
+        keys : ["resource", "get"],
+      },  //data.$get(this.id, {include: '*'})
+      params : [{
+        type : "scopeField", key : "id",
+      }, {
+        type : "constant", value :  {include: '*'},
+      }],
+      success : function(data){
+          this.item = data
+      },
+      fail : function(){
+        this.errors.push("There is no " + this.resource.descriptor.name + "with the id: " + this.id)
       }
     }
-    return stateLoaders
+}
+
+var cases = {
+  index : ["descriptor", "resource"],
+  new : ["descriptor", "resource"],
+  profile : ["descriptor", "resource", "item"],
+  edit : ["descriptor", "resource", "item"],
+}
+
+this.execute = function(patternType){
+
+  var calls = []
+  cases[patternType].forEach(function(call){
+    calls.push(functionStack[call])
+  })
+  var promises = []
+  calls.forEach((function(call){
+    var params = getParams(this, call.params)
+    var func = getFunction(this, call.call)
+    promises.push(this.call(params, func).then((call.success).bind(this), (call.fail).bind(this)))
+  }).bind(this))
+  Promise.all(promises)()
+}
+
+var getParams = function(scope, desc){
+  var params = []
+  desc.forEach(function(param){
+    switch(param.type){
+      case "scopeField" : params.push(scope[param.key]); break;
+      case "constant" : params.push(param.value); break;
+    }
+  })
+  return params
+}
+
+var getFunction = function(scope, desc){
+    switch(desc.type){
+        case "scopeField" :
+          var object = scope
+          desc.keys.forEach(function(key){
+            object = object[key]
+          });
+          return object
+        case "direct" : return desc.fcn;
+    }
+}
+
+this.call = function(params, func){
+  switch(params.length){
+    case 0 : return func();
+    case 1 : return func(params[0]);
+    case 2 : return func(params[0], params[1]);
+    case 3 : return func(params[0], params[1], params[2]);
+  }
 }
 */
+
+//this.execute("profile").bind(this)
