96c180d6ef87f73011d352e0d51d3bc229afd918
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-10-27 18:43:24 +0200

    item-list update. Template handles if there is no resource and the link works properly

--- src/components/cs-item-list/cs-item-list.js
+++ src/components/cs-item-list/cs-item-list.js
@@ -10,8 +10,8 @@ app.component("csItemList", {
   templateUrl : "components/cs-item-list/cs-item-list-template.html",
   controller : function(csRoute){
 
-    this.click = function(id){
-      csRoute.go("show", {id : id})
+    this.click = function(item){
+      csRoute.go("show", {resourceType : item.type, id : item.attributes.id})
     }
   }
 })
