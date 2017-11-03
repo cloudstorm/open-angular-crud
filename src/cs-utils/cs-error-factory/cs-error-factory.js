ead02a913b682a9886bab5d512cc553443e531ec
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-11-03 09:44:10 +0100

    cs-error-msg provider contains now only the message defintions. The assembly of the error messages is done by the cs-error-msg-factory.

---
+++ src/cs-utils/cs-error-factory/cs-error-factory.js
@@ -0,0 +1,64 @@
+'use strict'
+
+app = angular.module('cloudStorm.errorFactory', [])
+
+app.factory('csErrorFactory', ['csErrorMessages', function(csErrorMessages){
+
+  var get = function(componentName, type, params){
+
+    if(componentName in csErrorMessages.cases){
+      if(type in csErrorMessages.cases[componentName]){
+        var msg = csErrorMessages.cases[componentName][type].slice(0)
+        var reg = new RegExp("{{.*}}")
+        reg.exec(msg).forEach((function(inputDef){
+            msg = msg.replace(inputDef, this.getParam(inputDef, params))
+        }).bind(this))
+        return msg
+      } else {
+        return csErrorMessages.errorPrefix + type + ', there is no such case.';
+      }
+    } else {
+      return csErrorMessages.errorPrefix + componentName + ', there is no such component.';
+    }
+  }
+
+  var getParam = function(paramDef, params) {
+
+    if(params == null){
+      return csErrorMessages.errorPrefix + "'params' parameter is null!";
+    }
+    if((typeof params) != 'object'){
+      params = [params]
+    }
+    var def = paramDef.substring(2, paramDef.length - 2).split("|");
+    var num = parseInt(def[0]);
+    if(isNaN(num)) {
+      return csErrorMessages.errorPrefix + "'" + def[0] + "' is not an integer. Please revise the error msg definition";
+    }
+    var type = def[1];
+    var value = params[num];
+    switch(type){
+      case 'array' :
+        if(Array.isArray(value)){
+          var msg = '';
+          value.forEach(function(element){
+            msg += element + '\t'
+          })
+          return msg
+        } else {
+          return csErrorMessages.errorPrefix + 'Input param ' + num + ' is not an array';
+        }
+      case 'object':
+        //Comes later
+        return null
+      case undefined :
+        return value
+    }
+  }
+
+  return {
+    get: get,
+    getParam: getParam,
+  }
+
+}])
--- src/cs-utils/cs-error-msg/cs-error-msg-provider.js
+++ src/cs-utils/cs-error-msg/cs-error-msg-provider.js
@@ -2,69 +2,22 @@
 
 app = angular.module('cloudStorm.errorMsgProvider', [])
 
-app.provider('csErrorMsgs', [function() {
+app.provider('csErrorMessages', [function() {
 
   this.errorPrefix = "\ncsErrorMsgs ERROR: "
 
   this.cases = {
+
     'csDescriptorFactory': {
       'overlap': 'Overlapping data propagation definition in target: {{0|array}}'
-    }
-  }
-
-  this.get = function(componentName, type, params){
-
-    if(componentName in this.cases){
-      if(type in this.cases[componentName]){
-        var msg = this.cases[componentName][type].slice(0)
-        var reg = new RegExp("{{.*}}")
-        reg.exec(msg).forEach((function(inputDef){
-            msg = msg.replace(inputDef, this.getParam(inputDef, params))
-        }).bind(this))
-        return msg
-      } else {
-        return this.errorPrefix + type + ', there is no such case.';
-      }
-    } else {
-      return this.errorPrefix + componentName + ', there is no such component.';
-    }
-  }
-
-  this.getParam = function(paramDef, params) {
-
-    if(params == null){
-      return this.errorPrefix + "'params' parameter is null!";
-    }
-    if((typeof params) != 'object'){
-      params = [params]
-    }
-    var def = paramDef.substring(2, paramDef.length - 2).split("|");
-    var num = parseInt(def[0]);
-    if(isNaN(num)) {
-      return this.errorPrefix + "'" + def[0] + "' is not an integer. Please revise the error msg definition";
-    }
-    var type = def[1];
-    var value = params[num];
-    switch(type){
-      case 'array' :
-        if(Array.isArray(value)){
-          var msg = '';
-          value.forEach(function(element){
-            msg += element + '\t'
-          })
-          return msg
-        } else {
-          return this.errorPrefix + 'Input param ' + num + ' is not an array';
-        }
-      case 'object':
-        //Comes later
-        return null
-      case undefined :
-        return value
-    }
+    },
+    'test' : {
+      "1" : "ABCDEF : {{0|array}}"
+    },
   }
 
   this.$get = function() {
     return this;
   };
+
 }]);
