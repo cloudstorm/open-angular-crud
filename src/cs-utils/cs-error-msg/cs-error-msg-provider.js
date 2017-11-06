b289a65e7753a09e6a1f83fd0b07c7eac17a8233
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2017-11-06 11:49:07 +0100

    new error msg definitions

--- src/cs-utils/cs-error-msg/cs-error-msg-provider.js
+++ src/cs-utils/cs-error-msg/cs-error-msg-provider.js
@@ -9,7 +9,12 @@ app.provider('csErrorMessages', [function() {
   this.cases = {
 
     'csDescriptorFactory': {
-      'overlap': 'Overlapping data propagation definition in target: {{0|array}}'
+      'baseNotDefined' : 'Base variable is not defined for definition : \n {{0|array}}',
+      'intermediate' : 'From the array : \n {{0|array}} \n the key {{1}} does not refer to an object',
+      'overlap': 'Overlapping data propagation definition in target: {{0|array}}',
+    },
+    'general' : {
+
     },
     'test' : {
       "1" : "ABCDEF : {{0|array}}"
