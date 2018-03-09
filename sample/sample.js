1c0a6328913b39db1a883026e895b627d3072f97
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-03-09 19:27:39 +0100

    sample.js

--- sample/sample.js
+++ sample/sample.js
@@ -2,22 +2,22 @@ var app = angular.module('cloudStormSample', [
   'cloudStorm',
 ])
 
-app.controller('MainCtrl', function($scope, csAlertService, csDescriptorService, csRoute, $state) {
-  csDescriptorService.registerDescriptorUrl('resourceDescriptors/userResourceDescriptor.json');
-  csDescriptorService.registerDescriptorUrl('resourceDescriptors/categoryResourceDescriptor.json');
-  csDescriptorService.registerDescriptorUrl('resourceDescriptors/itemResourceDescriptor.json');
-
-  // // item resource is loaded with a delay, the cs-menu should reflect the newly loaded resource in 5 secs
-  // setTimeout(function(){ csDescriptorService.registerDescriptorUrl('resourceDescriptors/itemResourceDescriptor.json'); }, 5000);
-
-  csRoute.init();
-});
-
-
-// app.controller('MainCtrl', function($scope, csAlertService, csSettings, csDescriptorService, csRoute) {
-//   csDescriptorService.registerDescriptorUrl('http://web.csnodeapptemplate.docker/api/v1/descriptors');
-
-//   csAlertService.addAlert('Welcome to CloudStorm', 'info');
-//   csSettings.set('app-title', 'cs-node-app-template');
+// app.controller('MainCtrl', function($scope, csAlertService, csDescriptorService, csRoute, $state) {
+//   csDescriptorService.registerDescriptorUrl('resourceDescriptors/userResourceDescriptor.json');
+//   csDescriptorService.registerDescriptorUrl('resourceDescriptors/categoryResourceDescriptor.json');
+//   csDescriptorService.registerDescriptorUrl('resourceDescriptors/itemResourceDescriptor.json');
+//
+//   // // item resource is loaded with a delay, the cs-menu should reflect the newly loaded resource in 5 secs
+//   // setTimeout(function(){ csDescriptorService.registerDescriptorUrl('resourceDescriptors/itemResourceDescriptor.json'); }, 5000);
+//
 //   csRoute.init();
 // });
+
+
+app.controller('MainCtrl', function($scope, csAlertService, csSettings, csDescriptorService, csRoute) {
+  csDescriptorService.registerDescriptorUrl('http://web.csnodeapptemplate.docker/api/v1/descriptors');
+
+  csAlertService.addAlert('Welcome to CloudStorm', 'info');
+  csSettings.set('app-title', 'cs-node-app-template');
+  csRoute.init();
+});
