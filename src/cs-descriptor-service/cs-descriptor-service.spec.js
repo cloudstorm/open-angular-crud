1182b615f9abd6e301e2623563695a7a4986d8b7
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-03-27 14:32:18 +0200

    csDescriptorService exists in tests

---
+++ src/cs-descriptor-service/cs-descriptor-service.spec.js
@@ -0,0 +1,51 @@
+describe('csDescriptorService', function(){
+
+  var csDescriptorService, csResourceService;
+  var $q, $http;
+
+  beforeEach(angular.mock.module('cloudStorm.localizationProvider'));
+  beforeEach(angular.mock.module('cloudStorm.resourceService'));
+  beforeEach(angular.mock.module('cloudStorm.resource'))
+  beforeEach(angular.mock.module('cloudStorm.restApi'));
+  beforeEach(angular.mock.module('cloudStorm.dataStore'));
+  beforeEach(angular.mock.module('cloudStorm.settings'));
+  beforeEach(angular.mock.module('cloudStorm.descriptorService'))
+
+  // beforeEach(inject(function($injector) {
+  //     $q = $injector.get('$q');
+  //     $http = $injector.get('$http');
+  // }));
+
+  var civilization = {
+    "name": "civilization",
+    "attributes": [
+      { "name": "name", "type": "string", "required": true },
+      { "name": "kardashev_scale", "type": "string" },
+      { "name": "data", "type": "jsonb" }
+    ],
+    "relationships": [
+      { "name": "planets", "type": "has_many", "target": "planet", "foreign_key": "civilization_id" },
+      { "name": "home_planet", "type": "belongs_to", "target": "planet", "foreign_key": "home_planet_id" }
+    ],
+    "options": {
+      "paranoid": true
+    }
+  }
+
+
+  beforeEach(inject(function(_csDescriptorService_){
+    //console.log(_csDescriptorService_)
+    //csResourceService = _csResourceService_
+    csDescriptorService = _csDescriptorService_
+  }))
+
+  it('resourceService exists', function(){
+    expect(csDescriptorService).toBeDefined();
+  })
+
+  // it('csDescriptorService exists', function(){
+  //   expect(csResourceService).toBeDefined();
+  // })
+
+
+})
