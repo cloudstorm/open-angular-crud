2758f0f54c0f95770e94e8482a0923d182b90f6f
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-04-06 13:23:32 +0200

    component controller value test

--- src/components/cs-filter-row/cs-filter-row.spec.js
+++ src/components/cs-filter-row/cs-filter-row.spec.js
@@ -33,6 +33,14 @@ describe('csFilterRow', function(){
   //   expect(controller).toBeDefined()
   // })
 
+  var civilizationRaw = "{\"name\":\"Civilization\",\"type\":\"civilizations\",\"hint\":\"list\",\"base_url\":\"http://web.csnodeapptemplate.docker\",\"endpoint\":\"api/v1/civilizations\",\"fields\":[{\"attribute\":\"id\",\"cardinality\":\"one\",\"label\":\"Identifier\",\"read_only\":true,\"required\":false,\"type\":\"integer\"},{\"attribute\":\"name\",\"cardinality\":\"one\",\"label\":\"Name\",\"read_only\":false,\"required\":true,\"type\":\"string\"},{\"attribute\":\"kardashev_scale\",\"cardinality\":\"one\",\"label\":\"Kardashev Scale\",\"read_only\":false,\"required\":false,\"type\":\"string\"},{\"attribute\":\"data\",\"cardinality\":\"one\",\"label\":\"Data\",\"read_only\":false,\"required\":false,\"type\":\"code\"},{\"attribute\":\"planets\",\"cardinality\":\"many\",\"label\":\"Planets\",\"read_only\":false,\"required\":false,\"relationship\":\"planets\",\"resource\":\"planets\",\"type\":\"resource\"},{\"attribute\":\"home_planet_id\",\"cardinality\":\"one\",\"label\":\"Home Planet\",\"read_only\":false,\"required\":false,\"relationship\":\"home_planet\",\"resource\":\"planets\",\"type\":\"resource\"},{\"attribute\":\"created_at\",\"cardinality\":\"one\",\"label\":\"Created at\",\"read_only\":false,\"required\":false,\"type\":\"datetime\"},{\"attribute\":\"updated_at\",\"cardinality\":\"one\",\"label\":\"Updated at\",\"read_only\":false,\"required\":false,\"type\":\"datetime\"},{\"attribute\":\"deleted_at\",\"cardinality\":\"one\",\"label\":\"Deleted at\",\"read_only\":false,\"required\":false,\"type\":\"datetime\"}],\"attributes_to_hide\":{\"create\":[\"id\",\"created_at\",\"updated_at\",\"deleted_at\"],\"edit\":[\"id\",\"created_at\",\"updated_at\",\"deleted_at\"],\"index\":[\"deleted_at\"],\"show\":[]},\"display\":{\"name\":\"name\"}}";
+
+  var civilization = JSON.parse(civilizationRaw)
+
+  console.log("Civilization I")
+  console.log(JSON.stringify(civilization))
+
+
   var myService;
   var deferred;
   var csDescriptorService;
@@ -54,13 +62,37 @@ describe('csFilterRow', function(){
     csFilterRow = $componentController('csFilterRow', {
       $scope: scope,
       csDescriptorService: csDescriptorService,
-      $element: angular.element('<div></div>'),
+      $element: angular.element('<cs-filter-row></cs-filter-row>'),
     });
     csFilterRow.$onInit()
   }))
 
   it('getPromises called', function(){
     expect(csDescriptorService.getPromises).toHaveBeenCalled();
+  })
+
+  it('csFilterRow controller exists', function(){
+    expect(csFilterRow).toBeDefined();
+  })
+
+  // beforeEach(inject(function($rootScope, $componentController, $compile){
+  //
+  //   scope = $rootScope.$new()
+  //   csFilterRow = $componentController('csFilterRow', {
+  //     $scope: scope,
+  //     csDescriptorService: csDescriptorService,
+  //     $element: angular.element('<cs-filter-row></cs-filter-row>'),
+  //   }, {
+  //
+  //   });
+  //   csFilterRow.$onInit()
+  // }))
+
+  it('create resource', function(){
+
+    expect(csFilterRow.x).toBe("x")
+    //var civResource = csDescriptorService.registerDescriptor(civilization)
+    //expect(csResourceService.get("civilizations").descriptor.type).toEqual(civilization.type)
   })
 
   // var element, rootScope;
