cb1afc10ccc2676929a21fef20294a58f93ae7f2
kdaveed <konkolydavid@Konkolys-MacBook-Pro.local>
2018-04-09 16:22:49 +0200

    cs-alert-spec $compile test

--- src/components/cs-alert/cs-alert.spec.js
+++ src/components/cs-alert/cs-alert.spec.js
@@ -1,4 +1,5 @@
 describe('csAlert', function() {
+
   var $compile;
   var $rootScope;
   var csAlertService;
@@ -17,11 +18,16 @@ describe('csAlert', function() {
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
   beforeEach(inject(function(_$compile_, _$rootScope_, _csAlertService_){
+
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $compile = _$compile_;
     $rootScope = _$rootScope_;
     csAlertService = _csAlertService_;
   }));
+
+  it("compile defined", function(){
+    expect($compile).toBeDefined()
+  })
 
   it('Replaces the element with the appropriate content', function() {
     csAlertService.addAlert('test alert 1', 'success');
